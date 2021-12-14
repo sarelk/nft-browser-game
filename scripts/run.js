88c39fafae9982e88216e3b95e0ad84e65667be7
Sarel <Sarelk@gmail.com>
2021-12-14 19:50:16 +0200

    Boss & attack logic

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,16 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-      ["Leo", "Aang", "Pikachu"],       // Names
-      ["https://i.imgur.com/pKd5Sdk.png", // Images
-      "https://i.imgur.com/xVu4vFL.png", 
-      "https://i.imgur.com/WMB6g9u.png"],
-      [100, 200, 300],                    // HP values
-      [100, 50, 25]                       // Attack damage values
+        ["Angry Cat", "Rawring Cat", "Grumpy Cat"],       // Names
+        ["https://i.imgur.com/GPDDW1s.jpeg", // Images
+            "https://i.imgur.com/BXF7jhH.jpeg",
+            "https://i.imgur.com/jparT67.jpeg"],
+        [100, 200, 300],                    // HP values
+        [100, 50, 25],                       // Attack damage values
+        "Big Doge",
+        "https://i.imgur.com/HJQWbml.jpg",
+        10000,
+        50
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
@@ -14,31 +18,37 @@ const main = async () => {
     txn = await gameContract.mintCharacterNFT(0);
     await txn.wait();
     console.log("Minted NFT #1");
-  
+
     txn = await gameContract.mintCharacterNFT(1);
     await txn.wait();
     console.log("Minted NFT #2");
-  
+
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
     console.log("Minted NFT #3");
-  
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
     txn = await gameContract.mintCharacterNFT(1);
     await txn.wait();
     console.log("Minted NFT #4");
-  
+
     console.log("Done deploying and minting!");
-      
+
 };
-  
-  const runMain = async () => {
+
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+        await main();
+        process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  };
-  
-  runMain();
+};
+
+runMain();
